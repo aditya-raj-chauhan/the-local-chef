40b588059af73ee29ddedcb6199b8deecc4b8f1b
aditya raj chauhan <166774675+aditya-raj-chauhan@users.noreply.github.com>
2025-02-09 14:35:50 +0530

    Your commit message here

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter basename="/the-local-chef">
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
